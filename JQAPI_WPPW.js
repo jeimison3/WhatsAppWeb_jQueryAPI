90c1a5f06bab3ac3aa2dd5dd0e51127d89cd9522
Jeimison Moreno <jeimison.ti@gmail.com>
2018-10-22 19:58:16 -0300

    Funções de leitura de mensagens implementadas.

--- JQAPI_WPPW.js
+++ JQAPI_WPPW.js
@@ -14,13 +14,20 @@ class ChatListener{
     this._CHAT_USERNAME = this._CHAT_USER_DATA.children[0].children[0].children[0];
 
     this.USER_CHAT_SCR_TYPE = this.USER_CHAT_SCR.children[this.USER_CHAT_SCR_COUNT-2].children[0].children[1].children[0].children[1];
+
+    this.USER_CHAT_AREA = $(".copyable-area").children[2].children[2];
   }
 
   get chatName(){
     return this._CHAT_USERNAME.getAttribute('title');
   }
 
-
+  get receiveDivsMessages(){
+    var a = [];
+    for(var i=0;i<this.USER_CHAT_AREA.childElementCount;i++)
+      a.push(this.USER_CHAT_AREA.children[i]);
+    return a;
+  }
 
   clearMessage(){
     this.USER_CHAT_SCR_TYPE.textContent='';
@@ -40,5 +47,24 @@ class ChatListener{
     this.clearMessage();
     this.addMessage(msg);
     this.send();
+  }
+
+  get countChatMessages(){
+    return this.USER_CHAT_AREA.childElementCount;
+  }
+
+  decodeMessageId(id){
+    if( id >= this.countChatMessages() ) return -1; // Id inexistente
+    var obj_msg = this.receiveDivsMessages[id].lastElementChild.firstElementChild.firstElementChild;
+    if(obj_msg == null) return -1; // Mensagem ilegível
+    var envio = obj_msg.getAttribute("data-pre-plain-text");
+    var mensagem = obj_msg.children[0].children[0].textContent;
+
+    var envio = envio.split('] ');
+    var nome = envio[1].replace(':','');
+    var data_0 = envio[0].split('[')[1];
+    var hora_data = data_0.split(', ');
+
+    return [nome,mensagem,hora_data[1],hora_data[0]];
   }
 }
